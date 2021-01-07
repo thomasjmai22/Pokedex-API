9d45644088f9fd5e66d26f0dbe3be7963e049e31
thomasjmai22 <thomas.j.mai22@gmail.com>
2021-01-07 13:27:57 -0700

    update

--- server.js
+++ server.js
@@ -5,11 +5,40 @@ const app = express();
 
 app.use(morgan("dev"));
 
-app.use((req, res) => {
-  res.send("Hello World");
+app.use((req, res, next) => {
+  // res.send("Hello World");
+  next();
 });
 
 const PORT = 8000;
+const validTypes = [
+  `Bug`,
+  `Dark`,
+  `Dragon`,
+  `Electric`,
+  `Fairy`,
+  `Fighting`,
+  `Fire`,
+  `Flying`,
+  `Ghost`,
+  `Grass`,
+  `Ground`,
+  `Ice`,
+  `Normal`,
+  `Poison`,
+  `Psychic`,
+  `Rock`,
+  `Steel`,
+  `Water`,
+];
+
+//ROUTES
+app.get("/types", handleGetTypes);
+
+//FUNCTIONS
+function handleGetTypes(req, res) {
+  res.json(validTypes);
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
